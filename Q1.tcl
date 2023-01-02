98d7d0bb6478120424e1fa78227bcaf59699ab22
ishetabhatt <115787900+ishetabhatt@users.noreply.github.com>
2023-01-02 11:40:28 +0530

    Q1.tcl
    Fixed the code, formatted and added orientation.
    To add output pic still

--- Q1.tcl
+++ Q1.tcl
@@ -1,15 +1,30 @@
+#Create new simulator object
 set ns [new Simulator] 
-set namfile [open ex_01.nam w] 
+
+#Open nam tracefile
+set namfile [open Question1.nam w] 
 $ns namtrace-all $namfile 
-set tracefile [open ex_01.tr w] 
+
+#Open trace file
+set tracefile [open Question1.tr w] 
 $ns trace-all $tracefile 
+
+#Create nodes
 set n1 [$ns node] 
 set n2 [$ns node] 
 set n3 [$ns node] 
-set n4 [$ns node] 
+set n4 [$ns node]
+
+#Creating links between the nodes 
 $ns duplex-link $n1 $n2 1Mb 10ms DropTail 
 $ns duplex-link $n2 $n3 1Mb 10ms DropTail 
 $ns duplex-link $n2 $n4 1Mb 10ms DropTail 
+
+#Creating proper orientation of the nodes
+$ns duplex-link-op $n1 $n2 orient right
+$ns duplex-link-op $n2 $n3 orient up-right
+$ns duplex-link-op $n2 $n4 orient down-right
+
 set tcp [new Agent/TCP] 
 $ns attach-agent $n1 $tcp 
 set sink [new Agent/TCPSink] 
@@ -34,14 +49,19 @@ $ns at 0.3 "$cbr start"
 $ns at 0.3 "$ftp start" 
 $ns at 4.0 "$cbr stop" 
 $ns at 4.0 "$ftp stop" 
-proc finish {} 
-{ 
+
+#Finish procedure
+proc finish {} { 
 global ns namfile tracefile 
 $ns flush-trace 
 close $namfile 
 close $tracefile 
-exec nam ex_01.nam & 
+exec nam Question1.nam & 
 exit 0 
 } 
+
+#Calling finish operation after 10 seconds
 $ns at 10.0 "finish" 
+
+#Running simulation
 $ns run
